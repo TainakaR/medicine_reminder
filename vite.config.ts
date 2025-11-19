7e67585b89c6f8e5eed89334c668979f59357719
TainakaR <131835658+TainakaR@users.noreply.github.com>
2025-11-19 21:05:39 +0900

    package.json, vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc"
+import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-// https://vite.dev/config/
+const repositoryName = "react-todo-app";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.NODE_ENV === "production" ? `/${repositoryName}/` : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        404: path.resolve(__dirname, "404.html"),
+      },
+    },
+  },
   server: {
     port: 3000,
     strictPort: false,
